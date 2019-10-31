c51fcf5bfdeb06325e57740898372467356b7392
Ward Beyens <r0703044@student.thomasmore.be>
2019-10-31 01:01:08 +0100

    groeplocaties op de kaart zetten

--- www/js/plugin3.js
+++ www/js/plugin3.js
@@ -112,7 +112,7 @@ $('#getLocatiesOpKaart').click(function () {
 
             markerOpKaart = new L.marker([this.latitude, this.longitude], {icon: redIcon}).addTo(map)
                 .bindPopup("ID: " + this.locatieID + "<br>" + this.locatiebijnaam + "<br> lat: " + this.latitude + '<br>lng: ' + this.longitude).openPopup();
-            console.log( persoonID + " creeert een nieuwe marker met ID: " + this.locatieID);
+            console.log(persoonID + " creeert een nieuwe marker met ID: " + this.locatieID);
 
         });
     });
@@ -143,25 +143,25 @@ $('#getGroepen').click(function () {
     });
 });
 
+$('#closeGroepen').click(function () {
+    $('.emptylistGroup').empty();
+});
+
+
 // Get the modal
 var modal2 = document.getElementById("addUserToGroupModal");
-
 // Get the button that opens the modal
 var btn2 = document.getElementById("openAddUserToGroup");
-
 // Get the <span> element that closes the modal
 var span2 = document.getElementsByClassName("closeAddUserToGroup")[0];
-
 // When the user clicks the button, open the modal
 btn2.onclick = function () {
     modal2.style.display = "block";
 }
-
 // When the user clicks on <span> (x), close the modal
 span2.onclick = function () {
     modal2.style.display = "none";
 }
-
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
     if (event.target == modal2) {
@@ -173,6 +173,7 @@ $('#openAddUserToGroup').click(function () {
 
     console.log("U vraagt nu al alle groepen op");
     $('#addUserToGroupGroepID').empty();
+    $('#addUserToGroupGroepID').append(`<option value="" selected>Kies een groep</option>`);
 
     $.post('http://wabyte.com/getgroepen.php', function (data) {
         console.log(data);
@@ -190,6 +191,7 @@ $('#openAddUserToGroup').click(function () {
 
     console.log("U vraagt nu al uw opgeslagen locaties op met persoonID: " + persoonID);
     $('#addUserToGroupGroepYourLocation').empty();
+    $('#addUserToGroupGroepYourLocation').append(`<option value="" selected>Kies een locatie</option>`);
 
     $.post('http://wabyte.com/getlocaties.php', pars, function (data2) {
         console.log(data2);
@@ -204,10 +206,44 @@ $('#openAddUserToGroup').click(function () {
     });
 
 
-
 });
 
 $('#AddUserToGroup').click(function () {
+    let pars = {
+        persoonID: persoonID,
+        groepID: $('#addUserToGroupGroepID').val(),
+        locatieID: $('#addUserToGroupGroepYourLocation').val()
+    };
+    console.log(pars);
 
+    alert("U bent: " + persoonID + "\nU wilt in groep: " + $('#addUserToGroupGroepID').val() + "\nMet locatie: " + $('#addUserToGroupGroepYourLocation').val());
+    $.post('http://wabyte.com/addZitIN.php', pars, function (respons) {
+        console.log(respons);
+        alert("U bent toegevoegd aan een groep!"
+        );
+    });
 });
 
+$('#getGroepLocatiesOpKaart').click(function () {
+
+    let pars = {
+        persoonID: persoonID,
+    };
+    console.log("U vraagt de locaties van elk groeplid met persoonID: " + persoonID);
+
+    $.post('http://wabyte.com/getgroeplocaties.php', pars, function (data) {
+        console.log(data);
+        $.each(data, function (i) {
+            console.log(i);
+            // markersGroepOpKaart = new L.marker([this.latitude, this.longitude], {icon: blackIcon}).addTo(map);
+
+
+            markersGroepOpKaart = new L.marker([this.latitude, this.longitude], {icon: blackIcon}).addTo(map)
+                .bindPopup(this.voornaam + " (" + this.persoonID + ")<br>" + this.groepnaam + " (" + this.groepID + ")<br>" + this.locatiebijnaam + " (" + this.locatieID + ")").openPopup();
+            //console.log(persoonID + " creeert een nieuwe marker van de groep met ID: " + this.voornaam + " (" + this.persoonID + ")<br>" + this.groepnaam + " (" + this.groepID + ")<br>" + this.locatiebijnaam + " (" + this.locatieID + ")";
+
+
+        });
+    });
+
+});
