0a93595994821589286fc9152220c78949ddaa2e
Ward Beyens <r0703044@student.thomasmore.be>
2019-11-02 12:05:31 +0100

    kleurtjes

--- www/js/plugin02.js
+++ www/js/plugin02.js
@@ -9,6 +9,5 @@ window.fn.load = function(page) {
     var content = document.getElementById('content');
     var menu = document.getElementById('menu');
     content.load(page).then(menu.close.bind(menu));
-
-
 };
+
